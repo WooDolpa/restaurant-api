5c9e7f620b220bcc4856d866c18d9bf5799acb2c
jwlee <dannis1124@naver.com>
2023-01-24 22:26:56 +0900

    commit

--- src/main/java/com/restaurant/api/dto/login/LoginReqDto.java
+++ src/main/java/com/restaurant/api/dto/login/LoginReqDto.java
@@ -1,15 +1,24 @@
 package com.restaurant.api.dto.login;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 /**
  * packageName : com.restaurant.api.dto.login
  * className : LoginReqDto
  * user : jwlee
  * date : 2023/01/16
  */
+@Getter
+@ToString
+@NoArgsConstructor
 public class LoginReqDto {
 
     private String id;
     private String password;
+    @Builder
     public LoginReqDto(String id, String password) {
         this.id = id;
         this.password = password;
