f095161f46a9bf2f12e8ce2b4e5e1543bf3a1c20
jwlee <dannis1124@naver.com>
2023-01-28 23:29:35 +0900

    Service class 추가

---
+++ src/main/java/com/restaurant/api/service/AuthService.java
@@ -0,0 +1,13 @@
+package com.restaurant.api.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * packageName : com.restaurant.api.service
+ * className : AuthService
+ * user : jwlee
+ * date : 2023/01/28
+ */
+@Service
+public class AuthService {
+}
---
+++ src/main/java/com/restaurant/api/service/TableService.java
@@ -0,0 +1,13 @@
+package com.restaurant.api.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * packageName : com.restaurant.api.service
+ * className : TableService
+ * user : jwlee
+ * date : 2023/01/28
+ */
+@Service
+public class TableService {
+}
