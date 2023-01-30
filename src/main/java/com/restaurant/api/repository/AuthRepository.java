ce2f1736754464dab7a85dd140e5a6c186c93caf
jwlee <dannis1124@naver.com>
2023-01-30 23:59:20 +0900

    Repository 추가

---
+++ src/main/java/com/restaurant/api/repository/AuthRepository.java
@@ -0,0 +1,12 @@
+package com.restaurant.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * packageName : com.restaurant.api.repository
+ * className : AuthRepository
+ * user : jwlee
+ * date : 2023/01/30
+ */
+public interface AuthRepository {
+}
