4aabea811fa0c90e1396a5af7c71bddcfa341757
jwlee <dannis1124@naver.com>
2023-02-01 23:28:37 +0900

    Repository 추가

---
+++ src/main/java/com/restaurant/api/repository/StoreRepository.java
@@ -0,0 +1,10 @@
+package com.restaurant.api.repository;
+
+/**
+ * packageName : com.restaurant.api.repository
+ * className : StoreRepository
+ * user : jwlee
+ * date : 2023/02/01
+ */
+public interface StoreRepository {
+}
---
+++ src/main/java/com/restaurant/api/repository/TableRepository.java
@@ -0,0 +1,10 @@
+package com.restaurant.api.repository;
+
+/**
+ * packageName : com.restaurant.api.repository
+ * className : TableRepository
+ * user : jwlee
+ * date : 2023/02/01
+ */
+public interface TableRepository {
+}
